59b39249b3bc6f00990b2a365e914de5b013ff6a
weru7 <georgeweru143@gmail.com>
2022-09-23 13:54:11 -0700

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-if __name__ == "__main__":
-    arg_num = len(argv) - 1
-    if arg_num == 0:
-        print("{}".format(arg_num))
-    else:
-        result = []
-        for i in range(1, arg_num + 1):
-            result.append(int(argv[i]))
-            print("{}".format(sum(result)))
+if __name__ == '__main__':
+    from sys import argv
+    numb = 0
+    for index in argv[1:]:
+        numb += int(index)
+        print('{:d}'.format(numb))
