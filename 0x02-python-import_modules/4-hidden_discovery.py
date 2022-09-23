9d072982936d97d1c58502fb76b136d714524143
weru7 <georgeweru143@gmail.com>
2022-09-23 13:49:59 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     import hidden_4
-    all_dir = dir(hidden_4)
-    for i in range(0, len(all_dir)):
-        if "_" != all_dir[i][:2]:
-            print(all_dir[i])
+    for index in dir(hidden_4):
+        if index[:2] != '__':
+            print('{}'.format(index))
