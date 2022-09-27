59e8663331b3d1d020ef832f7e5cd7f081174fb0
weru7 <georgeweru143@gmail.com>
2022-09-27 04:53:23 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for index in range(0, len(my_string)):
         if my_string[index] != 'c' and my_string[index] != 'C':
             rev = rev + my_string[index]
-            return rev
+    return rev
