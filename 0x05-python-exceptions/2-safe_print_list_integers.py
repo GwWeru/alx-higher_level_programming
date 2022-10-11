f2502e302e07017766b1a3aa67dfb479959228c5
weru7 <georgeweru143@gmail.com>
2022-10-11 14:42:08 -0700

    safe print

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    cont = 0
+    for index in range(0, x):
+        try:
+            print('{:d}'.format(my_list[index]), end='')
+            cont = cont + 1
+        except (ValueError, TypeError):
+            pass
+        print()
+        return cont
