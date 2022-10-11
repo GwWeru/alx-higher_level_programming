bbedfdca7f1a9c5d1a4f416ce82731a0e9d7bcf3
weru7 <georgeweru143@gmail.com>
2022-10-11 14:44:09 -0700

    safe print

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             cont = cont + 1
         except (ValueError, TypeError):
             pass
-        print()
-        return cont
+    print()
+    return cont
