4e4f71100519941daf80d8abe473d04048f94680
weru7 <georgeweru143@gmail.com>
2022-10-11 14:47:57 -0700

    safe print

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+def safe_print_division(a, b):
+    total = 0
+    try:
+        total = a / b
+        return total
+    except ZeroDivisionError:
+        total = None
+        return total
+    finally:
+        print("Inside result: {}".format(total))
