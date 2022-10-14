4ff82b8b2026419a61ad8599ea89e2ecaf13c29f
weru7 <georgeweru143@gmail.com>
2022-10-14 12:56:43 -0700

    classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -18,3 +18,5 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self._size = size
