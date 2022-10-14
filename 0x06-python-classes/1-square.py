1f331e14824e18d59d5afe41ea0c0e30bccaee64
weru7 <georgeweru143@gmail.com>
2022-10-14 12:29:37 -0700

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Square class defination
+"""
+
+class Square:
+    """
+    Square class with private instance attribute size
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of the square
+        """
+        self.__size = size
