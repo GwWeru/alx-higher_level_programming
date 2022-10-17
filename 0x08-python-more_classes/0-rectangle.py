5bedf51d943de1ca2c293bb876aef9eca4464e8d
weru7 <georgeweru143@gmail.com>
2022-10-17 13:26:48 -0700

    classes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+
+This module defines a class - Rectangle
+
+"""
+
+
 class Rectangle:
     """
-    Creating an empty class that defines a rectangle
+    Creating an empty class
     """
     pass
