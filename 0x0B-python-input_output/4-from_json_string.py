7a0a9fdee6683db791a8f22587db9b8098ea5ce2
weru7 <georgeweru143@gmail.com>
2022-10-26 04:41:59 -0700

    json4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)
