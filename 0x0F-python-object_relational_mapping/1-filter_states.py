16368fb67a964b875b8192c98a7ed6be55e7305a
weru7 <georgeweru143@gmail.com>
2022-12-22 16:58:23 -0800

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ should take 3 arguments username, passwd and db name
 if __name__ == '__main__':
     import sys
     import MySQLdb
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states where name LIKE ''N%'\
                 ORDER BY id ASC")
