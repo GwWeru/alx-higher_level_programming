0f94fcdeff35106dc1d8a83ba6942c19fbaa6c52
weru7 <georgeweru143@gmail.com>
2023-01-18 00:08:30 -0800

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,6 @@ Takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
-
 import urllib.parse
 import urllib.request
 import sys
