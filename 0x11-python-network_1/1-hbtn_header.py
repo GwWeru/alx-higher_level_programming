0483c0c7bb18bdb2cb768cca055718066499f7f9
weru7 <georgeweru143@gmail.com>
2023-01-17 23:54:39 -0800

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
-from sys import argv
 """
-script that takes in a url and an email, sends a POST request
-to the url with the email as parameter and displays body of the
-response
+Displays the value of the X-Request-Id variable
+found in the header of the response
 """
 
+import urllib.request
+import sys
 
 if __name__ == "__main__":
-    """
-    """
-    info = {'email': argv[2]}
-    data = urllib.parse.urlencode(info)
-    data = data.encode('ascii')
-    url = argv[1]
-    reply = urllib.request.Request(url, data)
-    with urllib.request.urlopen(reply) as rep:
-        body = rep.read()
-    print(body.decode(encoding="utf-8"))
+    argument = sys.argv[1]
+    with urllib.request.urlopen(argument) as response:
+        print(response.headers['X-Request-Id'])
